a1943bc7a033eeef83905371ab8d81d4c692a02a
Wout <wout@woutmager.nl>
2019-05-21 15:10:49 +0200

    Adding titles and instructions

--- HappyDates/resources/lang/en/fieldsets/edit.php
+++ HappyDates/resources/lang/en/fieldsets/edit.php
@@ -8,78 +8,43 @@ return [
     'url' => 'Url',
 
     'enabled' => 'Enabled',
-    'enabled_instruct' => "If disabled, this feed won't check for new articles.",
+    'enabled_instruct' => "If disabled, this feed won't check for new events.",
 
     'scheduling' => 'Schedule',
     'scheduling_instruct' => 'Wait time between updates (minutes).',
 
-    'status' => 'What to do with new articles',
+    'status' => 'What to do with new events',
 
-    'cache_section' => 'Cache',
-
-    'cache' => "Use caching",
-    'cache_instruct' => "If disabled, this feed won't use caching. This might make fetching new articles a little slower. [More info](http://simplepie.org/wiki/faq/how_does_simplepie_s_caching_http_conditional_get_system_work)",
-
-    'cache_duration' => "Cache freshness",
-    'cache_duration_instruct' => "How long before asking for a fresh copy (seconds).",
-
-    'query_section' => 'Custom queries',
-
-    'query' => 'Add custom queries',
-
-    'query_grid' => 'Custom queries',
+    'collection_section' => 'Collection',
+    'collection_section_instruct' => 'To which collection should I add the events?',
 
     'content_section' => 'Content',
-    'content_section_instruct' => 'Where should the feed content go?',
+    'content_section_instruct' => 'If you use the presets the info integrates 100% with Prestige Worldwide',
 
-    'publish_to' => 'Choose a collection for new articles',
+    'publish_to' => 'Choose a collection for new events',
 
-    'item_title' => 'Article title',
-    'item_title_instruct' => 'Choose an existing field or add a custom fieldtype name.',
+    'date_section' => 'Event dates',
+    'date_section_instruct' => 'If you use the presets the info integrates 100% with Prestige Worldwide',
 
-    'item_description' => 'Article description',
-    'item_description_instruct' => 'Choose an existing field, add a custom fieldtype name, or disable saving the description.',
+    'pw_description' => 'Event description',
+    'pw_description_instruct' => 'Choose an existing field, add a custom fieldtype name, or disable saving the description.',
 
-    'item_content' => 'Article content',
-    'item_content_instruct' => 'Choose an existing field, add a custom fieldtype name, or disable saving the content.',
+    'pw_location' => 'Event location',
+    'pw_location_instruct' => 'Choose an existing field, add a custom fieldtype name, or disable saving the content.',
 
-    'item_permalink' => 'Article permalink',
-    'item_permalink_instruct' => 'Choose an existing field, add a custom fieldtype name, or disable saving the permalink.',
+    'pw_status' => 'Event status',
+    'pw_status_instruct' => 'Choose an existing field, add a custom fieldtype name, or disable saving the content.',
 
-    'authors_section' => 'Authors',
-    'authors_section_instruct' => 'Options for article authors.',
+    'pw_start_date' => 'Event start date',
+    'pw_start_date_instruct' => 'Choose an existing field, add a custom fieldtype name, or disable saving the content.',
 
-    'author_options' => 'Article authors',
-    'author_options_instruct' => 'Save the article author as a new user or attribute new articles to an existing one.',
+    'pw_end_date' => 'Event end date',
+    'pw_end_date_instruct' => 'Choose an existing field, add a custom fieldtype name, or disable saving the content.',
 
-    'item_authors' => 'Article author',
-    'item_authors_instruct' => 'Choose an existing field, add a custom fieldtype name, or disable saving the author.',
+    'pw_created' => 'Event created',
+    'pw_created_instruct' => 'Choose an existing field, add a custom fieldtype name, or disable saving the content.',
 
-    'item_author' => 'Choose a user',
-    'item_author_instruct' => 'To attribute new articles to.',
-
-    'taxonomies_section' => 'Taxonomies & terms',
-    'taxonomies_section_instruct' => 'Options for article categories.',
-
-    'item_taxonomies' => 'Article categories',
-    'item_taxonomies_instruct' => 'Which taxonomy should I use for article terms?',
-
-    'custom_taxonomies' => 'Custom term taxonomy',
-    'custom_taxonomies_instruct' => 'Which taxonomy should I use for custom terms?',
-
-    'custom_terms' => 'Custom terms',
-    'custom_terms_instruct' => 'These terms will be added to each new article.',
-
-    'images_section' => 'Images',
-    'images_section_instruct' => 'Options for article images.',
-
-    'item_thumbnail' => 'Article thumbnail',
-    'item_thumbnail_instruct' => 'Choose an existing field, add a custom fieldtype name, or disable saving the thumbnail.',
-
-    'save_images' => 'Save thumbnails',
-    'save_images_instruct' => 'Enable to download article thumbnails.',
-
-    'images_container' => 'Choose an asset container',
-    'images_container_instruct' => 'Article thumbnails will be downloaded and saved in the chosen asset container.',
+    'pw_updated' => 'Event updated',
+    'pw_updated_instruct' => 'Choose an existing field, add a custom fieldtype name, or disable saving the content.',
 
 ];
--- HappyDates/resources/lang/nl/messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-
-    'source_suggest_placeholder' => 'Kies of typ een veldnaam die de waarde bevat.',
-    'no_collections' => 'Er zijn geen collecties.',
-    'no_taxonomies' => 'Er zijn geen taxonomieën.',
-
-];
